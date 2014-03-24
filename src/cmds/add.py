876132b62130413377214d27c38cb0d5225ecb8d
Mike <stark3@gmail.com>
2014-03-24 15:54:03 -0400

    Fixing !add.

--- src/cmds/add.py
+++ src/cmds/add.py
@@ -1,6 +1,10 @@
+import config
 import honor_functions
+from functions import is_registered
 
-def add(components):
+def add(socket, components):
+    if not components['sender'] in config.owner or not is_registered(socket, components['sender']):
+        return 'This command can be run only by the owners!'.encode('utf8')
     arguments = components['arguments'].split('!add ')[1].split(':')
     phrase = arguments[0]
     lower_phrase = phrase.lower()
