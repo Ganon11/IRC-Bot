8eb3e28ccdb44662c78a60220ec546bbf17ca982
Mike <stark3@gmail.com>
2014-04-26 15:41:20 -0400

    Attempted to use bibles.org. This was a mistake.

--- src/cmds/bible.py
+++ src/cmds/bible.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup
 import json
 import os
 import requests
@@ -7,6 +8,7 @@ BIBLES_ORG_API_KEY = ''
 BIBLES_ORG_API_PASS = 'X'
 BIBLES_ORG_API_VER = 'v2'
 BIBLES_ORG_CHAPTERS = 'chapters'
+BIBLES_ORG_PASSAGES = 'passages'
 BIBLES_ORG_URL = 'https://bibles.org'
 BIBLES_ORG_VERSES = 'verses'
 BIBLES_ORG_VERSIONS = 'versions'
@@ -111,7 +113,7 @@ def LoadApiKey():
 
 def GetVerse(verse, lang=DEFAULT_LANGUAGE, version=DEFAULT_VERSION):
 	'''
-	Gets the text of the requested verse(s), in the requested language/translation.
+	Gets the text of the requested verse, in the requested language/translation.
 
 	verse is a 5-value tuple:
 	(Book name, start chapter, start verse, end chapter, end verse)
@@ -127,10 +129,90 @@ def GetVerse(verse, lang=DEFAULT_LANGUAGE, version=DEFAULT_VERSION):
 		'chapter': verse[1],
 		'verse': verse[2]
 	}
-	print 'Making request to ' + str(request_url)
 	resp = MakeRequest(request_url)
-	print 'Got response ' + str(resp.text)
+	soup = BeautifulSoup(json.loads(resp.text)['response']['verses'][0]['text'])
+	return '%(chapter)s:%(verse)s| %(text)s' % {
+		'chapter': verse[1],
+		'verse': verse[2],
+		'text': soup.p.text.replace(soup.p.sup.text, '', 1)
+	}
+
+def GetPassage(verse_spec, lang=DEFAULT_LANGUAGE, version=DEFAULT_VERSION):
+	'''
+	Gets the text of the requested verses, in the requested language/translation.
+
+	verse is a 5-value tuple:
+	(Book name, start chapter, start verse, end chapter, end verse)
+	'''
+	LoadApiKey()
+	passage_specifiers = []
+	if verse_spec[1] == verse_spec[3]: # Staying within same chapter
+		passage_specifiers.append('%(book)s+%(chap)s:%(start)s-%(end)s' % {
+			'book': verse_spec[0],
+			'chap': verse_spec[1],
+			'start': verse_spec[2],
+			'end': verse_spec[4]
+		})
+	else: # More than 1 chapter
+		passage_specifiers.append('%(book)s+%(chap)s:%(start)s-ff' % {
+			'book': verse_spec[0],
+			'chap': verse_spec[1],
+			'start': verse_spec[2]
+		})
+		for i in range(verse_spec[1] + 1, verse_spec[3]):
+			passage_specifiers.append('%(book)s+%(chap)s' % {
+				'book': verse_spec[0],
+				'chap': i
+			})
+		passage_specifiers.append('%(book)s+%(chap)s:1-%(end)s' % {
+			'book': verse_spec[0],
+			'chap': verse_spec[3],
+			'end': verse_spec[4]
+		})
+	request_url = '%(url_base)s/%(api_ver)s/%(action)s.js?version=%(language)s-%(translation)s&q[]=%(passage_specifier_list)s' % {
+		'url_base': BIBLES_ORG_URL,
+		'api_ver': BIBLES_ORG_API_VER,
+		'action': BIBLES_ORG_PASSAGES,
+		'language': lang,
+		'translation': version,
+		'passage_specifier_list': ','.join(passage_specifiers)
+	}
+	print "URL: '%s'" % request_url
+	resp = MakeRequest(request_url)
+	chap_num = verse_spec[1]
+	result = []
+	for passage in json.loads(resp.text)['response']['search']['result']['passages']:
+		soup = BeautifulSoup(passage['text'])
+		v_nums = []
+		for p in soup.find_all('p'):
+			print str(p)
+			text = p.text
+			for v in p.find_all('sup'):
+				print str(v)
+				v_nums.append(v.text)
+				text = text.replace(v.text, '<>', 1)
+			print v_nums
+			text = text[2:] # Strip first <>
+			while '<>' in text:
+				pos = text.find('<>')
+				result.append('%(chapter)s:%(verse)s| %(text)s' % {
+					'chapter': chap_num,
+					'verse': v_nums.pop(0),
+					'text': text[:pos]
+				})
+				text = text[pos + 2:]
+			result.append('%(chapter)s:%(verse)s| %(text)s' % {
+				'chapter': chap_num,
+				'verse': v_nums.pop(0),
+				'text': text
+			})
+		chap_num = chap_num + 1
+
+	return '\r\n'.join(result)
 
 if __name__ == '__main__':
-	myVerse = ('Romans', 1, 1, 1, 10)
-	GetVerse(myVerse)
+	myVerse = ('Acts', 1, 19, 1, 20)
+	if myVerse[1] == myVerse[3] and myVerse[2] == myVerse[4]:
+		print GetVerse(myVerse).encode('utf8')
+	else:
+		print GetPassage(myVerse).encode('utf8')
