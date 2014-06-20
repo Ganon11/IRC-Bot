d7adf5167b81610699112fe20ebd9560bf301811
Michael Stark <stark3@gmail.com>
2014-06-20 14:52:41 -0400

    Check self heresy if no target is given.

--- src/cmds/heretic.py
+++ src/cmds/heretic.py
@@ -2,11 +2,15 @@ import heretic_functions
 
 def heretic(components):
 	response = ''
-	target = components['arguments'].split('!heretic ')[1]
+	target = ''
+	try:
+		target = components['arguments'].split('!heretic ')[1]
+	except:
+		target = components['sender']
 	count = heretic_functions.get_heretic(target)
 	response = '%(target)s (%(count)s denunciation%(plural)s)' % { 'target': target, 'count': count, 'plural': '' if count == 1 else 's'}
 	return response.encode('utf8')
 
 if __name__ == '__main__':
-	comp = { 'arguments': '!heretic aletheia' }
+	comp = { 'arguments': '!heretic', 'sender': 'mstark' }
 	print heretic(comp)
