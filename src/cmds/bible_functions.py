51c00e99def7d565ea3062d40f597939271aa461
Michael Stark <stark3@gmail.com>
2014-07-15 13:35:46 -0400

    Start trying to use bibles.org as the source of data.
    Still a lot of work to do.

--- src/cmds/bible_functions.py
+++ src/cmds/bible_functions.py
@@ -4,7 +4,9 @@
 # Publishers. Used by permission. All rights reserved. Text provided by the
 # Crossway Bibles Web Service (http://www.gnpcb.org/esv/share/services/).
 
+from bs4 import BeautifulSoup
 from pythonbible import Passage
+import json
 import os
 import re
 import requests
@@ -66,6 +68,50 @@ def GetPassage(verse):
    verses = ['[' + RemoveExtraWhitespace(v.strip()) for v in re.split(r'\[', formatted_text)[1:] if v.strip() != '']# First entry in verses is an empty string. Skip it.
    return '\r\n'.join(verses)
 
+################################################################################
+# Attempt to get bibles.org working. Thanks to _joe from IRC for some of the
+# initial work!
+################################################################################
+
+BIBLES_KEY = ''
+BIBLES_URL = 'https://bibles.org/v2/passages.js'
+BIBLES_DEFAULT_PARAMS = {
+   'version': 'eng-ESV'
+}
+
+def MakeBiblesRequest(data):
+   print data
+   LoadBiblesKey()
+   par = BIBLES_DEFAULT_PARAMS
+   par['q[]'] = data
+   return requests.get(BIBLES_URL, params=par, auth=requests.auth.HTTPBasicAuth(BIBLES_KEY, 'X'))
+
+def LoadBiblesKey():
+   global BIBLES_KEY
+   if BIBLES_KEY == '':
+      api_key_file = open(os.path.join(os.getcwd(), '..', '..', 'files', 'bibles_org_api_key.dat'))
+      BIBLES_KEY = api_key_file.read()
+      api_key_file.close()
+
+def GetPassage2(verse):
+   passageSpec = '%(book)s+%(chap_start)s:%(v_start)s-%(chap_end)s:%(v_end)s' % {
+      'book': verse[0],
+      'chap_start': verse[1],
+      'v_start': verse[2],
+      'chap_end': verse[3],
+      'v_end': verse[4]
+   }
+   text = json.loads(MakeBiblesRequest(passageSpec).text)['response']['search']['result']['passages'][0]['text']
+   text = re.sub(r'<sup( \w+="[\w\d\.]+")+>\d+</sup>', ' ', text)
+   text = re.sub(r'<h3( \w+="[\w\d\.]+")+>[\w\s]+</h3>', ' ', text)
+   text = re.sub(r'<span( \w+="[\w\d\.]+")+>([\w\s]+)</span>', r'\2', text)
+   text = text.replace('*', '')
+   text = text.replace('\n', ' ')
+   soup = BeautifulSoup(text)
+   verses = [ x for x in soup.stripped_strings ]
+   return '\r\n'.join(verses).encode('utf-8')
+
 if __name__ == "__main__":
    verse = ('John', 3, 16, 3, 16)
-   print GetPassage(verse)
+   result = GetPassage2(verse)
+   #print result
